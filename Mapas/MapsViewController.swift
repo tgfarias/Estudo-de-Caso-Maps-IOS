04bf4f1d6fb249abd98bfc7a360d9f40b9055fa6
fedega <fedegakgb@gmail.com>
2017-03-05 09:26:52 -0300

    Setando meu local atual através do gerente GPS

--- Mapas/MapsViewController.swift
+++ Mapas/MapsViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapsViewController: UIViewController, MKMapViewDelegate {
+class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    var gerenteGPS:CLLocationManager!
+    var localizacaoAtual: CLLocation!
     @IBOutlet weak var vrMapa: MKMapView!
     @IBAction func vrSegment(_ sender: UISegmentedControl)
     {
@@ -27,6 +30,7 @@ class MapsViewController: UIViewController, MKMapViewDelegate {
             vrMapa.mapType = .standard
         }
     }
+    
     @IBAction func trataGesto(_ sender: UILongPressGestureRecognizer)
     {
         if (sender.state != .began)
@@ -42,14 +46,56 @@ class MapsViewController: UIViewController, MKMapViewDelegate {
         pino.coordinate = coordenada
         pino.title = "Algum lugar que eu curto?"
         vrMapa.addAnnotation(pino)
+        gerenteGPS.stopUpdatingLocation()
+    }
+
+    @IBAction func btnGerarRota(_ sender: UIButton) {
+        if let origem = localizacaoAtual
+        {
+        
+            let destino = CLLocationCoordinate2D(latitude:-10.270148, longitude: -48.331896)
+            //gera os pontos de marcacoes para
+            //origem e destino
+            let origemPlaceMark = MKPlacemark(coordinate: origem.coordinate, addressDictionary: nil)
+            let destinoPlaceMark = MKPlacemark(coordinate: destino, addressDictionary: nil)
+            
+            let origemMapItem = MKMapItem(placemark: origemPlaceMark)
+            
+            let destinoMapItem = MKMapItem(placemark: destinoPlaceMark)
+            //configura o objeto q busca as direcoes do caminho
+            //para o tipo de transporte desejado
+            let direcoes  = MKDirectionsRequest()
+            direcoes.source = origemMapItem
+            direcoes.destination = destinoMapItem
+            direcoes.transportType = .automobile
+            
+            
+            
+        }
     }
     
+    @IBAction func btnLocAtual(_ sender: UIButton) {
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            //solicita autorizacao do usuario
+            gerenteGPS.requestWhenInUseAuthorization()
+            //inicializa o procedimento de busca pelas localizacoes
+            gerenteGPS.startUpdatingLocation()
+        }
+        else
+        {
+            print ("HABILITE SEU GPS")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         vrMapa.delegate = self
-        // Do any additional setup after loading the view, typically from a nib.
-        let destino = CLLocationCoordinate2D(latitude: -10.270148, longitude: -48.331896)
-        let zoom = MKCoordinateSpanMake(0.01, 0.01)
+        
+        gerenteGPS = CLLocationManager();
+        gerenteGPS.delegate = self
+        
+        let destino = CLLocationCoordinate2D(latitude: -10.1845667, longitude: -48.3357847)
+        let zoom = MKCoordinateSpanMake(0.03, 0.03)
         let regiao = MKCoordinateRegionMake(destino, zoom)
         vrMapa.setRegion(regiao, animated: true)
     }
@@ -59,15 +105,44 @@ class MapsViewController: UIViewController, MKMapViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+    /*func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         let pinoPerson = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
         pinoPerson.image = UIImage(named: "images")
         pinoPerson.canShowCallout = true
         
         return pinoPerson
+    }*/
+ 
+    //Funcao chamada para buscar as coordenadas
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let posicaoAtual = locations.last
+        {
+            
+            //criacao do pino q indicará o local
+            let pino = MKPointAnnotation()
+            pino.coordinate = posicaoAtual.coordinate
+            pino.title = "Estou aqui"
+            vrMapa.addAnnotation(pino)
+            
+            // criacao da regiao para onde vai a camera
+            let zoom = MKCoordinateSpanMake(0.01, 0.01)
+            
+            //regiao para onde sera enviado a camera
+            let regiao = MKCoordinateRegionMake(posicaoAtual.coordinate, zoom)
+            //apontando a regiao no mapa
+            vrMapa.setRegion(regiao, animated: true)
+            
+            
+            gerenteGPS.stopUpdatingLocation()
+        }
     }
     
-    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.red
+        renderer.lineWidth = 4.0
+        return renderer
+    }
 
 
 }
